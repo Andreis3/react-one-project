71aae680213eea1c81d38fb1bd129cb81918de3c
Andreis3 <andrei.as@hotmail.com>
2019-07-31 01:13:31 -0300

    techitem create

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import TechItem from "./TechItem";
 
 class TechList extends Component {
   state = {
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
-  }
-  render(){
+    newTech: "",
+    techs: ["Node.js", "ReactJS", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  };
+  render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
   }
 }
 
