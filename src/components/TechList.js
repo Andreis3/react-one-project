98a4f9b26e9ceea0a9dd480863ef0cd10739741f
Andreis3 <andrei.as@hotmail.com>
2019-07-31 23:31:00 -0300

    create props react

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,8 +5,32 @@ import TechItem from "./TechItem";
 class TechList extends Component {
   state = {
     newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: []
   };
+
+  //executado sempre assim que o compomente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if(techs){
+      this.setState({ techs: JSON.parse(techs)})
+    }
+  };
+
+  // executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    // this.props, this.state sempre que houver alterações
+    if(prevState.techs !== this.state.techs){
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+
+
+  };
+
+  //   //executado quando o compomente deixa de existir
+  // componentWillUnmount() {
+
+  // };
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
